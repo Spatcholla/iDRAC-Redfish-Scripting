9bf6243710112fba77b577a5cfefdf66f12d21a6
Samuel Moore <samuel@odinware.com>
2019-11-25 02:23:06 -0600

    Still testing

--- redfish_python_async/ImportSystemConfigurationLocalFilenameREDFISH.py
+++ redfish_python_async/ImportSystemConfigurationLocalFilenameREDFISH.py
@@ -1,21 +1,3 @@
-#
-# ImportSystemConfigurationLocalFilenameREDFISH. Python script using Redfish API to import system configuration profile attributes locally from a configuration file.
-#
-# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 8.0
-#
-# Copyright (c) 2017, Dell, Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-#
-
-from datetime import datetime
-
 import asyncio
 import json
 import logging
@@ -24,9 +6,6 @@ import re
 import sys
 import time
 from typing import IO
-
-import urllib.error
-import urllib.parse
 
 import aiofiles
 import aiohttp
@@ -46,16 +25,174 @@ async def main(
         ip: str,
         username: str,
         password: str,
-        script_examples: str,
         target: str,
         shutdown: str,
-        filename: str,
+        config_file: str,
         end_state: str,
-        file: IO
+        file: IO,
 ) -> None:
     """Crawl & write concurrently to `file` for multiple `urls`."""
-    config = get_config(filename)
+    job = locals()
 
+    config = get_config(config_file)
+
+    auth = aiohttp.BasicAuth(login=username, password=password)
+
+    async with ClientSession(auth=auth) as job['session']:
+        post_results = await post_config(**job)
+        job.update(**post_results)
+        if "job_id" in job:
+            status_results = await parse_status(**job)
+            job.update(**status_results)
+        await write_status(**job)
+
+
+async def write_status(file: IO, **kwargs) -> None:
+    """Write the status of job from `node` to `file`."""
+    print(kwargs)
+    # if not res:
+    #     return None
+    # async with aiofiles.open(file, "a") as f:
+    #     for p in res:
+    #         await f.write(f"{url}\t{p}\n")
+    #     logger.info(f"Wrote results for source URL: {url}")
+
+
+async def parse_status(ip: str, session: ClientSession, job_id: str, **kwargs) -> dict:
+    """Find HREFs in the HTML of `url`."""
+    job = dict()
+    while True:
+        try:
+            response = await get_status(ip=ip, job_id=job_id, session=session)
+        except (
+            aiohttp.ClientError,
+            aiohttp.ClientConnectionError,
+        ) as e:
+            logger.error(
+                f"aiohttp exception for {ip} [{getattr(e, 'status', None)}]: {getattr(e, 'message', None),}"
+            )
+            return job
+        except Exception as e:
+            logger.exception(
+                f"Non-aiohttp exception occured: {getattr(e, '__dict__', {})}"
+            )
+            return job
+        else:
+            status = response.status
+            data = response.json()
+            if status != 202 or 200:
+                logger.info(f"FAIL -- Got response [{response.status}] for {ip}")
+                await asyncio.sleep(10)
+                continue
+
+            fail_messages = [
+                "failed",
+                "completed with errors",
+                "Not one",
+                "not compliant",
+                "Unable",
+                "The system could not be shut down",
+                "No device configuration"
+            ]
+
+            success_messages = [
+                "Successfully imported",
+                "completed with errors",
+                "Successfully imported",
+            ]
+
+            reboot_messages = [
+                "No reboot Server",
+            ]
+
+            no_change_messages = [
+                "No changes",
+                "No configuration changes",
+            ]
+
+            def job_state(states: list, message: str) -> list:
+                return [state for state in states if state in message]
+
+            if any(job_state(states=fail_messages, message=data['Oem']['Dell']['Message'])):
+                logger.info(
+                    f"FAIL -- {job['job_id']} marked as {data[u'Oem'][u'Dell'][u'JobState']} but detected issue(s). "
+                    f"See detailed job results below for more information on failure\n"
+                    f"Detailed job results for {job['job_id']}\n"
+                    f"{data['Oem']['Dell']}\n"
+                    f"{data['Messages']}\n"
+                )
+                job['status'] = "failed"
+                return job
+
+            elif any(job_state(states=reboot_messages, message=data['Oem']['Dell']['Message'])):
+                logger.info(
+                    f"REBOOT -- job ID {job['job_id']} successfully marked completed. NoReboot value detected and "
+                    f"config changes will not be applied until next manual server reboot\n"
+                    f"Detailed job results for {job['job_id']}\n"
+                    f"{data['Oem']['Dell']}\n"
+                    f"{data['Messages']}\n"
+                )
+                job['status'] = "reboot needed"
+                return job
+
+            elif any(job_state(states=success_messages, message=data['Oem']['Dell']['Message'])):
+                end = time.perf_counter_ns()
+                completion_time = (end - job['start']) / 1e9
+
+                logger.info(
+                    f"SUCCESS -- job ID {job['job_id']} successfully marked completed\n"
+                    f"Detailed job results for job ID {job['job_id']}\n"
+                    f"{data['Oem']['Dell']}\n"
+                    f"{job['job_id']} completed in: {completion_time:.02f} seconds\n"
+                    f"Config results for job ID {job['job_id']}\n"
+                    f"{data['Messages']}\n"
+                )
+                job['end'] = end
+                job['completion_time'] = completion_time
+                job['status'] = 'completed'
+                return job
+
+            elif any(job_state(states=no_change_messages, message=data['Oem']['Dell']['Message'])):
+                logger.info(
+                    f"NO CHANGE -- job ID {job['job_id']} marked completed\n"
+                    f"Detailed job results for job ID {job['job_id']}\n"
+                )
+                job['status'] = 'no change'
+                return job
+
+            else:
+                logger.info(
+                    f"STATUS -- JobStatus not completed, current status: {data['Oem']['Dell']['Message']}, "
+                    f"percent complete: {data['Oem']['Dell']['PercentComplete']}"
+                )
+                await asyncio.sleep(10)
+                continue
+
+
+async def get_status(
+        ip: str,
+        session: ClientSession,
+        job_id: str,
+) -> aiohttp.ClientResponse:
+    response = await session.get(
+        url=f"https://{ip}/redfish/v1/TaskService/Tasks/{job_id}",
+        ssl=False,
+    )
+    return response
+
+
+async def post_config(
+        session: ClientSession,
+        ip: str,
+        target: str,
+        shutdown: str,
+        config: str,
+        end_state: str,
+        node: str,
+        **kwargs: dict,
+) -> dict:
+    """POST request wrapper to push iDRAC configuration.
+    """
     url = f"https://{ip}/redfish/v1/Managers/iDRAC.Embedded.1/Actions/Oem/EID_674_Manager.ImportSystemConfiguration"
 
     payload = {"ImportBuffer": "", "ShareParameters": {"Target": target}}
@@ -66,66 +203,6 @@ async def main(
 
     payload["ImportBuffer"] = config
     headers = {"content-type": "application/json"}
-    auth = aiohttp.BasicAuth(login=username, password=password)
-
-    async with ClientSession(auth=auth) as session:
-        tasks = []
-        tasks.append(
-            write_status(file=file, url=url, session=session, headers=headers, payload=payload)
-        )
-        await asyncio.gather(*tasks)
-
-
-# async def parse(url: str, session: ClientSession, **kwargs) -> set:
-#     """Find HREFs in the HTML of `url`."""
-#     found = set()
-#     try:
-#         html = await fetch_html(url=url, session=session, **kwargs)
-#     except (
-#         aiohttp.ClientError,
-#         aiohttp.http_exceptions.HttpProcessingError,
-#     ) as e:
-#         logger.error(
-#             f"aiohttp exception for {url} [{getattr(e, 'status', None)}]: {getattr(e, 'message', None),}"
-#         )
-#         return found
-#     except Exception as e:
-#         logger.exception(
-#             "Non-aiohttp exception occured:  %s", getattr(e, "__dict__", {})
-#         )
-#         return found
-#     else:
-#         for link in HREF_RE.findall(html):
-#             try:
-#                 abslink = urllib.parse.urljoin(url, link)
-#             except (urllib.error.URLError, ValueError):
-#                 logger.exception("Error parsing URL: %s", link)
-#                 pass
-#             else:
-#                 found.add(abslink)
-#         logger.info("Found %d links for %s", len(found), url)
-#         return found
-
-async def write_status(file: IO, url: str, **kwargs) -> None:
-    """Write the found response from `url` to `file`."""
-    res = await post_config(url=url, **kwargs)
-    print(res)
-    # if not res:
-    #     return None
-    # async with aiofiles.open(file, "a") as f:
-    #     for p in res:
-    #         await f.write(f"{url}\t{p}\n")
-    #     logger.info(f"Wrote results for source URL: {url}")
-
-
-async def post_config(
-        url: str,
-        session: ClientSession,
-        payload: str,
-        headers: dict,
-) -> tuple:
-    """POST request wrapper to push iDRAC configuration.
-    """
 
     response = await session.post(
         url=url,
@@ -134,119 +211,28 @@ async def post_config(
         ssl=False,
     )
     response.raise_for_status()
-    logger.info(f"Got response [{response.status}] for URL: {url}")
+    logger.info(f"Got response [{response.status}] for {node}")
 
     try:
         job_id = re.search("JID_\d+", str(response)).group()
-        print(job_id)
     except:
         print(f"\n- FAIL: status code {response.status} returned")
         print(f"- Detailed error information: {response}")
 
-    # job_id = re.sub("[,']", "", z)
-    print(type(response.status))
-    if int(response.status) != 202:
-        print(f"\n- FAIL, status code not 202\n, code is: {response.status}")
-        logger.info(f"FAIL -- Got response [{response.status}] for URL: {url}")
+    if response.status != 202:
+        logger.info(f"FAIL -- Got response [{response.status}] for {node}")
     else:
         print(f"\n- {job_id} successfully created for ImportSystemConfiguration method\n")
-        logger.info(f"FAIL -- Got response [{response.status}] for URL: {url}")
+        logger.info(f"SUCCESS -- {job_id} successfully created for {node}")
 
-    start_time = datetime.now()
+    start = time.perf_counter_ns()
 
-    return job_id, start_time
+    job = {
+        'job_id': job_id,
+        'start': start,
+    }
 
-
-# async def get_job_status(
-#     ip: str, username: str, password: str, job_id, start_time,
-# ) -> None:
-#     while True:
-#         req = requests.get(
-#             f"https://{ip}/redfish/v1/TaskService/Tasks/{job_id}",
-#             auth=(username, password),
-#             verify=False,
-#         )
-#         status = req.status
-#         data = req.json()
-#         current_time = datetime.now() - start_time
-#         if status == 202 or status == 200:
-#             pass
-#             time.sleep(3)
-#         else:
-#             print(f"Query job ID command failed, error code is: {status}")
-#             sys.exit()
-#         if (
-#             "failed" in data["Oem"]["Dell"]["Message"]
-#             or "completed with errors" in data["Oem"]["Dell"]["Message"]
-#             or "Not one" in data["Oem"]["Dell"]["Message"]
-#             or "not compliant" in data["Oem"]["Dell"]["Message"]
-#             or "Unable" in data["Oem"]["Dell"]["Message"]
-#             or "The system could not be shut down" in data["Oem"]["Dell"]["Message"]
-#             or "No device configuration" in data["Oem"]["Dell"]["Message"]
-#         ):
-#             print(
-#                 f"- FAIL, Job ID {job_id} marked as {data[u'Oem'][u'Dell'][u'JobState']} but detected issue(s). "
-#                 f"See detailed job results below for more information on failure\n"
-#             )
-#             print(f"- Detailed job results for job ID {job_id}\n")
-#             for i in data["Oem"]["Dell"].items():
-#                 print(f"{i[0]}: {i[1]}")
-#             print(f"\n- Config results for job ID {job_id}\n")
-#             for i in data["Messages"]:
-#                 for ii in i.items():
-#                     if ii[0] == "Oem":
-#                         print("-" * 80)
-#                         for iii in ii[1]["Dell"].items():
-#                             print(f"{iii[0]}: {iii[1]}")
-#                     else:
-#                         pass
-#             sys.exit()
-#         elif "No reboot Server" in data["Oem"]["Dell"]["Message"]:
-#             print(
-#                 f"- PASS, job ID {job_id} successfully marked completed. NoReboot value detected and config changes will "
-#                 f"not be applied until next manual server reboot\n"
-#             )
-#             print(f"\n- Detailed job results for job ID {job_id}\n")
-#             for i in data["Oem"]["Dell"].items():
-#                 print(f"{i[0]}: {i[1]}")
-#             sys.exit()
-#         elif (
-#             "Successfully imported" in data["Oem"]["Dell"]["Message"]
-#             or "completed with errors" in data["Oem"]["Dell"]["Message"]
-#             or "Successfully imported" in data["Oem"]["Dell"]["Message"]
-#         ):
-#             print(f"- PASS, job ID {job_id} successfully marked completed\n")
-#             print(f"- Detailed job results for job ID {job_id}\n")
-#             for i in data["Oem"]["Dell"].items():
-#                 print(f"{i[0]}: {i[1]}")
-#             print(f"\n- {job_id} completed in: {str(current_time)[0:7]}")
-#             print(f"\n- Config results for job ID {job_id}\n")
-#             for i in data["Messages"]:
-#                 for ii in i.items():
-#                     if ii[0] == "Oem":
-#                         print("-" * 80)
-#                         for iii in ii[1]["Dell"].items():
-#                             print(f"{iii[0]}: {iii[1]}")
-#                     else:
-#                         pass
-#
-#             sys.exit()
-#         elif (
-#             "No changes" in data["Oem"]["Dell"]["Message"]
-#             or "No configuration changes" in data["Oem"]["Dell"]["Message"]
-#         ):
-#             print(f"\n- PASS, job ID {job_id} marked completed\n")
-#             print(f"- Detailed job results for job ID {job_id}\n")
-#             for i in data["Oem"]["Dell"].items():
-#                 print(f"{i[0]}: {i[1]}")
-#             sys.exit()
-#         else:
-#             print(
-#                 f"- WARNING, JobStatus not completed, current status: {data['Oem']['Dell']['Message']}, "
-#                 f"percent complete: {data['Oem']['Dell']['PercentComplete']}"
-#             )
-#             await asyncio.sleep(1)
-#             continue
+    return job
 
 
 def get_config(filename: str) -> str:
@@ -273,15 +259,15 @@ if __name__ == "__main__":
     parser.add_argument("-ip", help="iDRAC IP address", required=True)
     parser.add_argument("-u", "--username", help="iDRAC username", required=True)
     parser.add_argument("-p", "--password", help="iDRAC password", required=True)
-    parser.add_argument(
-        "script_examples",
-        action="store_true",
-        help="ImportSystemConfigurationLocalFilenameREDFISH.py -ip 192.168.0.120 -u root -p calvin -t ALL "
-             "-f SCP_export_R740, this example is going to import SCP file and apply all attribute changes "
-             "for all components. \nImportSystemConfigurationLocalFilenameREDFISH.py -ip 192.168.0.120 "
-             "-u root -p calvin -t BIOS --filename R740_scp_file -s Forced, this example is going to only "
-             "apply BIOS changes from the SCP file along with forcing a server power reboot.",
-    )
+    # parser.add_argument(
+    #     "script_examples",
+    #     action="store_true",
+    #     help="ImportSystemConfigurationLocalFilenameREDFISH.py -ip 192.168.0.120 -u root -p calvin -t ALL "
+    #          "-f SCP_export_R740, this example is going to import SCP file and apply all attribute changes "
+    #          "for all components. \nImportSystemConfigurationLocalFilenameREDFISH.py -ip 192.168.0.120 "
+    #          "-u root -p calvin -t BIOS --filename R740_scp_file -s Forced, this example is going to only "
+    #          "apply BIOS changes from the SCP file along with forcing a server power reboot.",
+    # )
     parser.add_argument(
         "-t",
         "--target",
